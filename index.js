34899040898276fc04658f69e398404c06ae9188
ReC82 <lloyd.malfliet@gmail.com>
2018-06-19 17:20:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ restService.post("/movie", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   
-  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-  var xhr = new XMLHttpRequest();
+  //var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  //var xhr = new XMLHttpRequest();
   //req.open('GET', 'http://www.allocine.fr/recherche/?q=' + speech, false);   
   //req.send(null);  
  /* if(req.status == 200)  
